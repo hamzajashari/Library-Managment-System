71d9d33e7f7cbca14edf96e387e7751c0f78128a
Hamza <hamzajashari1234@gmail.com>
2022-04-20 21:51:43 +0200

    rework

---
+++ src/main/java/com/mnb/exception/LibraryCartException.java
@@ -0,0 +1,12 @@
+package com.mnb.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class LibraryCartException extends RuntimeException{
+
+    public LibraryCartException(Long id) {
+        super(String.format("Shopping cart with id: %d was not found", id));
+    }
+}
